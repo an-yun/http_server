47d1543fe5682959c5a0ca677f45e93b07e86e4d
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-03-21 17:01:59 +0800

    测试include

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "make update" << std::endl;
+    printf("test include");
     return 0;
 }
