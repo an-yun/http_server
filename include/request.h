308e338c8401e1aa2136a38e023d7c18016b6a00
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-05-16 16:20:52 +0800

    添加request定义

---
+++ include/request.h
@@ -0,0 +1,48 @@
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include <string>
+#include <cstddef>
+#include <limits>
+#include <map>
+
+enum RequestMethod
+{
+    GET,
+    POST
+};
+
+enum ConnectionType
+{
+    KeepAlive,
+    Upgrade
+};
+class Request
+{
+    public:
+        using size_t  = std::size_t;
+        const static size_t SIZE_MAX;
+        using string  = std::string;
+        template<class K, class V> using map = std::map<K,V>;
+
+    private:
+        enum RequestMethod method;
+        string request_url;
+        string http_type;
+        string host;
+        string user_agent;
+        string content_type;
+        size_t content_length;
+        enum ConnectionType connection;
+
+    public:
+        Request(const char * requst_str, size_t n = SIZE_MAX) ;
+        Request(const string & requst_str);
+        
+        string get_request_path();
+        map<string, string> get_request_parameter();
+        enum RequestMethod get_request_type();
+        
+};
+
+#endif
---
+++ src/request.cpp
@@ -0,0 +1,8 @@
+#include "request.h"
+
+const Request::size_t Request::SIZE_MAX = std::numeric_limits<Request::size_t>::max();
+
+Request::Request(const char * requst_str, size_t n)
+{
+
+}
